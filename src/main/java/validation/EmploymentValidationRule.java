d81291c31fb90316d4fdc33f4128099cf3462142
gorelov-m-v <mixer880@yandex.ru>
2023-06-03 14:38:09 +0300

    validation

---
+++ src/main/java/validation/EmploymentValidationRule.java
@@ -0,0 +1,13 @@
+package validation;
+
+public class EmploymentValidationRule extends ValidationRule<Boolean> {
+
+    public EmploymentValidationRule(Boolean value) {
+        super(value, "Ипотека выдается только трудоустроенным");
+    }
+
+    @Override
+    public boolean isValid() {
+        return value;
+    }
+}
---
+++ src/main/java/validation/MortgageAmountValidationRule.java
@@ -0,0 +1,17 @@
+package validation;
+
+public class MortgageAmountValidationRule extends ValidationRule<Integer> {
+
+    public MortgageAmountValidationRule(Integer value) {
+        super(value, "Минимальный размер ипотеки - 1.000.000, а максимальный - 10.000.000");
+    }
+
+    @Override
+    public boolean isValid() {
+        return value >= 1000000 && value <= 10000000;
+    }
+
+    // Объявите и реализуйте метод для проверки суммы ипотеки ниже
+
+
+}
