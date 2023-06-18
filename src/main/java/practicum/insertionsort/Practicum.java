2a83c19a40ddba92712116f6657d0d4e553ff602
gorelov-m-v <mixer880@yandex.ru>
2023-06-18 17:59:31 +0300

    binaryquest

---
+++ src/main/java/practicum/insertionsort/Practicum.java
@@ -0,0 +1,27 @@
+package practicum.insertionsort;
+
+import java.util.Arrays;
+
+public class Practicum {
+
+    public static void main(String[] args) {
+        int[] array = {7, 4, 2, 0, 1, 3, 4, 9};
+        System.out.println(Arrays.toString(array));
+
+        insertionSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array [j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+}
