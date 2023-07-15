ec771b3e075e62f9c13bda7b3d963d504182470f
gorelov-m-v <mixer880@yandex.ru>
2023-07-15 23:58:14 +0300

    PrimeNumbers

--- src/main/java/practicumsterams/inputNumbers/Practicum.java
+++ src/main/java/practicumsterams/inputNumbers/Practicum.java
@@ -8,7 +8,7 @@ public class Practicum {
     public static void main(String[] args) {
         List<Integer> inputNumbers = List.of(2, 5, 4, 2, 3, 8);
         List<Integer> evenNumbers = new ArrayList<>();
-        
+
         inputNumbers.stream()
                 .filter(number -> number % 2 == 0)
                 .forEach(evenNumbers::add);
