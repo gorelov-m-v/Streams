6983c5d79780abae4ff349a91bff026daa3f7c47
gorelov-m-v <mixer880@yandex.ru>
2023-07-20 22:33:48 +0300

    watch

---
+++ src/main/java/data/units/DiscountCalculator.java
@@ -0,0 +1,12 @@
+package data.units;
+
+public class DiscountCalculator {
+
+    public int sumAfterDiscount(int sum) {
+        if (sum < 1000) {
+            return sum;
+        } else {
+            return (int) (sum * 0.98);
+        }
+    }
+}
